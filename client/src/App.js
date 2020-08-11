5dcab69399b780bc638bb309ccceb1d032a9d4b1
Aldana Castro <aldanacastro1999@gmail.com>
2020-08-11 12:40:50 -0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/css/App.css';
+import Router from './Router';
+function App() {
+  return (
+    <div className="App">
+        <Router/>
+    </div>
+  );
+}
+
+export default App;
